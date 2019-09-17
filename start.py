99e8b755257e981a3ce620bab657f5d1aa317e96
Anil Sapkota <45574896+anilsapkota@users.noreply.github.com>
2019-09-17 07:19:11 -1000

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-img = cv2.imread('../images/devon.jpg')
+img = cv2.imread('../images/anil.jpg') #this is path to the image file 
 #print(img.shape)
 
 b = img[:,:,0] #all of the row , all of the coloumn , first array as 0
@@ -12,6 +12,8 @@ r = img[:,:,2]
 cv2.imshow('Blue', b)
 cv2.imshow('Green',g)
 cv2.imshow('red', r)
-cv2.imshow('Image', img)
+
+cv2.imshow('Image', img) #this command displays the Image read from the imread command 
+
 cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.destroyAllWindows() 
